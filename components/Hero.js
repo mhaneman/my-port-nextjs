cd5710d4948e5ab0a855b18bdbb9f93e5ac8e25b
michaelhaneman <mhaneman@calpoly.edu>
2022-10-15 01:53:34 -0700

    game preview mock

--- components/Hero.js
+++ components/Hero.js
@@ -6,16 +6,22 @@ import MyWorkToggle from './elements/MyWorkToggle'
 export default function Hero() {
   return (
     <div style={{backgroundImage: `url(hero-bg.jpg)`}}
-      className="relative overflow-hidden h-screen bg-no-repeat bg-cover bg-center bg-fixed">
+      className="relative overflow-hidden h-screen bg-no-repeat bg-cover bg-center bg-fixed
+        grid grid-rows-3 justify-center">
 
         <DarkModeToggle />
         <MyWorkToggle />
 
-        <h4>Image Produced using Blender</h4>
 
-        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Source Code
-        </button>
+        <div className=''>
+          <h4>Image Generated using Blender</h4>
+
+          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border 
+            border-blue-500 hover:border-transparent rounded">
+            Script
+          </button>
+        </div>
+        
     </div>
   )
 }
